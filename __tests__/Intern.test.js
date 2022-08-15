1dcbd1e37256d77f9771b3db6e561f72fab127b9
kristafrancis721 <kristafrancis08@yahoo.com>
2022-08-15 18:22:20 -0400

    add code to intern test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,19 +1,30 @@
 const Intern = require("../lib/Intern");
 
-test("Can set school via constructor", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.school).toBe(testValue);
+const intern = new Intern('Jackson', '72113', 'jsal721@gmail.com', 'UCF');
+
+test('test to see if we can get the constuctor values for the manager object', () => {
+    expect(intern.name).toBe('Jackson');
+    expect(intern.id).toBe('72113');
+    expect(intern.email).toBe('jsal721@gmail.com');
+    expect(intern.school).toBe('UCF');
 });
 
-test("getRole() should return \"Intern\"", () => {
-  const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
-  expect(e.getRole()).toBe(testValue);
+test('test to see if we can get the name from the getName() method', () => {
+    expect(intern.getName()).toBe('Jackson');
 });
 
-test("Can get school via getSchool()", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
-  expect(e.getSchool()).toBe(testValue);
+test('test to see if we can get the id from the getId() method', () => {
+    expect(intern.getId()).toBe('72113');
+});
+
+test('test to see if we can get the email from the getEmail() method', () => {
+    expect(intern.email()).toBe('jsal721@gmail.com');
+});
+
+test('test to see if we get the school from the getSchool() method'), () => {
+    expect(intern.school()).toBe('UCF');
+}
+
+test('test to see if we can get the role from the getRole() method', () => {
+    expect(intern.getRole()).toBe('intern');
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,20 +1,29 @@
 const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+const manager = new Manager('Jackson', '72113', 'jsal721@gmail.com', '721');
 
-test("Can set office number via constructor argument", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
+test('test to see if we can get the constuctor values for the manager object', () => {
+    expect(manager.name).toBe('Jackson');
+    expect(manager.id).toBe('72113');
+    expect(manager.email).toBe('jsal721@gmail.com');
+    expect(manager.officeNumber).toBe('721');
 });
 
-test("getRole() should return \"Manager\"", () => {
-  const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
-  expect(e.getRole()).toBe(testValue);
+test('test to see if we can get the name from the getName() method', () => {
+    expect(manager.getName()).toBe('Jackson');
 });
 
-test("Can get office number via getOffice()", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
+test('test to see if we can get the id from the getId() method', () => {
+    expect(manager.getId()).toBe('72113');
+});
+
+test('test to see if we can get the email from the getEmail() method', () => {
+    expect(manager.email).toBe('jsal721@gmail.com');
+});
+
+test('test to see if we get the office number from the getOfficeNumber() method'), () => {
+    expect(manager.getOfficeNumber()).toBe('821');
+}
+
+test('test to see if we can get the role from the getRole() method', () => {
+    expect(manager.getRole()).toBe('Manager');
 });
