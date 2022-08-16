b4726cac7a6e1a014979987c98579913d94b6b68
kristafrancis721 <kristafrancis08@yahoo.com>
2022-08-16 17:56:23 -0400

    add css and HTML

--- src/generate-site.js
+++ src/generate-site.js
@@ -91,27 +91,19 @@ module.exports = team => {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    <link rel="stylesheet" href="<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">"
+    <link rel="stylesheet" href="../dist/style.css">
+    <script src="https://kit.fontawesome.com/ed9dbe6958.js" crossorigin="anonymous"></script>
+    <title>Team Profile Generator</title>
 </head>
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
-                ${generateTeam(team)}
-            </div>
-        </div>
-    </div>
+    <header>
+    <h1>My Team</h1>
+    </header>
+           <main> ${generateTeam(team)} </main>
+           
 </body>
 </html>
     `;
-};
+}
