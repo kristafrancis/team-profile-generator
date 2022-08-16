46bacbf4bd8d18ca19924b9bbe9d16d77ad77a69
kristafrancis721 <kristafrancis08@yahoo.com>
2022-08-16 16:49:31 -0400

    add HTML for manger, engineer and intern

--- src/generate-site.js
+++ src/generate-site.js
@@ -1,83 +1,85 @@
 // create the team
 const generateTeam = team => {
+    console.log(team);
+
+    //Create an empty array to push html elements and loop through team data
+    const html = [];
 
     // create the manager html
     const generateManager = manager => {
-        return `
-        <div class="card employee-card">
+        console.log(manager);
+        let managerHTML = `
+        <div class="card" style="width: 18rem;">
         <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+        ${manager.name} <br/>
+            <i class="fa-mug-hot"></i>Manager</div>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${manager.id}</li>
+            <li class="list-group-item>Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
+            <li class="list-group-item>Office Number: ${manager.officeNumber}</li>
             </ul>
-        </div>
-    </div>
-        `;
-    };
+            </div>
+             `;
+             html.push(managerHTML);
+    }
 
     // create the html for engineers
     const generateEngineer = engineer => {
-        return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-        </ul>
-    </div>
-</div>
-        `;
-    };
+        console.log(engineer);
+        let engineerHTML = `
+        <div class="card" style="width: 18rem;">
+        <div class="card-header">
+        ${engineer.name} <br/>
+            <i class="fas fa-glasses"></i>Engineer</div>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${engineer.id}</li>
+            <li class="list-group-item>Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
+            <li class="list-group-item>Github Username: <a target="_blank" href="https://github.com/${engineer.githubUsername}">${engineer.githubUsername}</a></li>
+            </ul>
+            </div>
+             `;
+             html.push(engineerHTML);
+    }
 
     // create the html for interns
     const generateIntern = intern => {
-        return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.getSchool()}</li>
-        </ul>
-    </div>
-</div>
-        `;
-    };
+        console.log(intern);
+        let internHTML = `
+        <div class="card" style="width: 18rem;">
+        <div class="card-header">
+        ${intern.name} <br/>
+            <i class="fas fa-user-graduate"></i>Intern</div>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${intern.id}</li>
+            <li class="list-group-item>Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+            <li class="list-group-item>School: ${intern.school}</li>
+            </ul>
+            </div>
+             `;
+             html.push(internHTML);
+    }
 
-    const html = [];
+    
 
-    html.push(team
-        .filter(employee => employee.getRole() === "Manager")
-        .map(manager => generateManager(manager))
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Engineer")
-        .map(engineer => generateEngineer(engineer))
-        .join("")
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Intern")
-        .map(intern => generateIntern(intern))
-        .join("")
-    );
+    //create loop for all of the employees
+    for (let i = 0; i < team.length; i++) {
+        
+        if (team[i].getRole() === "Manager") {
+            generateManager(team[i]);
+        }
+        if (team[i].getRole() === "Engineer") {
+            generateManager(team[i]);
+        }
+        if (team[i].getRole() === "Intern") {
+            generateManager(team[i]);
+        }
+    }
 
-    return html.join("");
+    //join HTML blocks
+    return html.join('');
 
 }
+
 
 // export function to generate entire page
 module.exports = team => {
@@ -87,10 +89,9 @@ module.exports = team => {
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
